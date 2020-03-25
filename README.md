6e87d96a57318b9788cfbaa43b646780584adf3a
Ashley Waldner <ashleywaldner22@gmail.com>
2020-03-25 09:29:53 -0500

    fix html, fix radiobuttons, display questions/answers, add if to determine correct answers

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ THE GAME CODE
 2. set up multiple answer where the user can choose the awswer
 3. set a fuction that determines the correct answer
 4. set a timer that counts down from 30 sec - when the timer runs out the answers are revealed and the game resets
-5. set a function that determines how many question where anwers correctly/incorrectly and push the results to the screen.
+the timer should hide the start page and when it runs out the results page should show up
+5. set a function that determines how many question where anwers correctly/incorrectly or missed and push the results to the screen.
+6. there needs to be a done button for if the questions are answered before the timer goes off 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-
      // array of objects with the questions, possible answers, and the correct answer
   var questionBank =
   [
@@ -34,40 +32,130 @@ $(document).ready(function(){
       answers: ["Sticky Shoes", "Smelly Cat", "The Cow in the Meadow", "Parading Goats"],
       correct: "Smelly Cat"
     },
-  ]
 
-   // functions to handle the building questions page and scoring
+    {
+      question: "Who's catchphrase is 'How you doin'?",
+      answers: ["Chandler", "Ross", "Phoebe", "Joey"],
+      correct: "Joey"
+    },
+  ];
+
+
+  $(document).ready(function(){
+
+    $("#timer").hide()
+    
+    // start the game when user clicks on Start button
+    $("#start-button").on("click", gameState.startTimer);
+    
+  });
+  
+  // information about the state of game play
+  var gameState = {
+   
+    // set the time at 60 seconds, and count down by 1 second
+    timeRemaining : 60,
+  
+    // start the timer, hide the start page, show the questions
+    startTimer: function() {
+      $("#timer").show()
+      $("#timer").text(gameState.timeRemaining);
+      setInterval(gameState.countdown, 1000);
+      $("#start-page").hide();
+      trivia.displayQuestions();
+    },
+  
+    // decrement the timer and update the UI; stop the timer at 0
+    countdown: function() {
+      gameState.timeRemaining--;
+      $("#timer").text(gameState.timeRemaining);
+      if (gameState.timeRemaining === 0) {
+        gameState.stopTimer();
+        $("#timer").empty();
+      }
+    },
+  
+    // stop the timer and check the answers
+    stopTimer: function() {
+      clearInterval();
+      trivia.checkAnswers();
+    },
+  
+    // hide the quetions and display the end page with results
+    showEndPage: function(numCorrect, numIncorrect, numUnanswered) {
+      $("#end-page").show();
+      $("#questions-box").empty();
+      $("#timer").empty();
+      $("#timer").hide();
+      $("#correct-answers").text("Correct answers: " + numCorrect);
+      $("#incorrect-answers").text("Incorrect answers: " + numIncorrect);
+      $("#unanswered").text("Skipped questions: " + numUnanswered);
+    }
+  }
+  
+  // functions to handle the building questions page and scoring
   var trivia = {
   
     // pull questions from the array of questions, loop through them, and append to UI
     displayQuestions: function() {
       var divContainer = $("#questions-box");
       var answerGroup = $(".form-check");
-      divContainer.append('<h2>Answer the following questions:</h2>');
+      divContainer.append('<h2>How well do you know Friends?</h2>');
               
       for (var i = 0; i < questionBank.length; i++) {
   
-        // Display the questions to the users screen
+        // send questions to users screen
         divContainer.append('<div id="question">' + questionBank[i].question + '</div>');
   
-        // Declare vars for each one of the possibe answers
+        // Declare vars for possible answers
         var answer1 = questionBank[i].answers[0];
         var answer2 = questionBank[i].answers[1];
         var answer3 = questionBank[i].answers[2];
         var answer4 = questionBank[i].answers[3];
-  
-        // Show the answers as radio buttons
+
+        // Make answers radio buttons
         divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer1 + '</label></div>');
         divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer2 + '</label></div>');
         divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer3 + '</label></div>');
         divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer4 + '</label></div>');
+    
       }
   
-      // Done Button for when the user finishes before the time is up 
-      var doneButton = '<button class="btn btn-primary" id="done-button" type="submit">Done</button>';
+      // add a Done button to the end of the page and register its click handler
+      var doneButton = '<button id="done-button" type="submit">Done</button>';
       divContainer.append(doneButton);
       $("#done-button").on("click", gameState.stopTimer);
     },
   
-   
- 
+    // test if the user answers are correct, incorrect, or if there are unanswered questions
+    checkAnswers: function() {
+      var correctAnswer;
+      var userAnswer;
+      var numCorrect = 0;
+      var numIncorrect = 0;
+      var numUnanswered = 0;
+  
+      // loop through to compare the text of the label with the user answers
+      // increment score counts appropriately
+      for (var i = 0; i < questionBank.length; i++) {
+        correctAnswer = questionBank[i].correct;
+        userAnswer = $('input[id=radio'+i+']:checked + label').text();
+  
+        if (userAnswer === correctAnswer) {
+          numCorrect++;
+        } else if (userAnswer === "") {
+          numUnanswered++;
+        } else if (userAnswer !== correctAnswer) {
+          {
+            numIncorrect++;
+          }
+        }
+      }
+  
+      // show the end page with the score tally
+      gameState.showEndPage(numCorrect, numIncorrect, numUnanswered);
+    },
+  }
+
+
+  
